8af68083f790c6b9a0211cf2a8a99c7d9e12e82b
Derek Parker <parkerderek86@gmail.com>
2022-06-07 16:12:02 -0700

    Fixup patch & script

--- scripts/setup-initial-patch.sh
+++ scripts/setup-initial-patch.sh
@@ -26,22 +26,28 @@ git submodule update
 # Enter the submodule directory.
 cd ./go
 
+ORIGINAL_GIT_SHA=$(git rev-parse HEAD)
+
 # Apply the initial patch. This patch is basic and shouldn't accrue many
 # conflicts over time so it should be safe to apply.
 git apply ../patches/000-initial-setup.patch
+# Add the initial changes to the index so the later diff ignores them.
+git add .
+git commit -m phase1
 
 # Apply some manual substitutions with sed. These changes will likely introduce
 # merge conflicts if this was a patch, so we do them here instead and generate a patch
 # after.
 GO_SOURCES=src/crypto/**/*.go
-sed -i -e "s/boring\.Enabled/boring\.Enabled()/g" ${GO_SOURCES}
-sed -i -e "s/\"crypto\/internal\/boring\"/boring \"crypto\/internal\/backend\"/g" ${GO_SOURCES}
-sed -i -e "s/const boringEnabled/var boringEnabled/g" ${GO_SOURCES}
+sed -i -e "s/boringCert(t, \"R2\", boringRSAKey(t, 4096), nil, boringCertCA)/boringCert(t, \"R2\", boringRSAKey(t, 4096), nil, boringCertCA|boringCertFIPSOK)/g" src/crypto/tls/boring_test.go
 sed -i -e "s/boring.SignRSAPKCS1v15(bkey, hash, hashed)/boring.SignRSAPKCS1v15(bkey, hash, hashed, true)/g" src/crypto/rsa/pkcs1v15.go
 sed -i -e "s/boring.VerifyRSAPKCS1v15(bkey, hash, hashed, sig)/boring.VerifyRSAPKCS1v15(bkey, hash, hashed, sig, hash != crypto.Hash(0))/g" src/crypto/rsa/pkcs1v15.go
 sed -i -e "s/boring.SignMarshalECDSA(b, digest)/boring.SignMarshalECDSA(b, digest, crypto.Hash(0))/g" src/crypto/ecdsa/ecdsa.go
 sed -i -e "s/boring.SignECDSA(b, hash)/boring.SignECDSA(b, hash, crypto.Hash(0))/g" src/crypto/ecdsa/ecdsa.go
 sed -i -e "s/boring.VerifyECDSA(b, hash, r, s)/boring.VerifyECDSA(b, hash, r, s, crypto.Hash(0))/g" src/crypto/ecdsa/ecdsa.go
+sed -i -e "s/boring\.Enabled/boring\.Enabled()/g" ${GO_SOURCES}
+sed -i -e "s/\"crypto\/internal\/boring\"/boring \"crypto\/internal\/backend\"/g" ${GO_SOURCES}
+sed -i -e "s/const boringEnabled/var boringEnabled/g" ${GO_SOURCES}
 
 # Remove the crypto/internal/boring code as we're replacing it with the openssl backend code.
 rm src/crypto/internal/boring/*.*
@@ -54,8 +60,8 @@ go mod tidy
 go mod vendor
 
 # Generate the final patch.
-git diff > ../../patches/001-initial-openssl-for-fips.patch
+git add .
+git diff --cached --binary > ../../patches/001-initial-openssl-for-fips.patch
 
 # Clean things up again after we've generated the patch.
-git checkout .
-git clean -fd
+git reset --hard ${ORIGINAL_GIT_SHA}
