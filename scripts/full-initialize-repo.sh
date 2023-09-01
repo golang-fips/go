9a96f777421b4f4aca67920e7e9ec6d93cfc8f1c
Derek Parker <parkerderek86@gmail.com>
2023-09-01 07:33:42 -0700

    Print go version and env when initializing repo
    This could be helpful to track down issues, but mostly pushing this to
    trigger a new CI run.

--- scripts/full-initialize-repo.sh
+++ scripts/full-initialize-repo.sh
@@ -3,6 +3,12 @@
 # This script generates and applies FIPS
 # patches to a Go tree.
 
+echo "Host Go Version:"
+go version
+
+echo "Host Go Env:"
+go env
+
 SCRIPT_DIR=$(readlink -f $(dirname $0))
 GO_DIR=${SCRIPT_DIR}/../go
 
