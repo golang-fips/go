6ea4cb9250263ff23ac43dc76988f889e5fbb63b
Derek Parker <parkerderek86@gmail.com>
2023-10-10 15:42:08 -0700

    Support openssl/v2 module

--- scripts/create-secondary-patch.sh
+++ scripts/create-secondary-patch.sh
@@ -6,8 +6,6 @@ set -ex
 # merge conflicts if this was a patch, so we do them here instead and generate a patch
 # after.
 GO_SOURCES=src/crypto/**/*.go
-sed -i -e "s/boring.SignRSAPKCS1v15(bkey, hash, hashed)/boring.SignRSAPKCS1v15(bkey, hash, hashed, true)/g" src/crypto/rsa/pkcs1v15.go
-sed -i -e "s/boring.VerifyRSAPKCS1v15(bkey, hash, hashed, sig)/boring.VerifyRSAPKCS1v15(bkey, hash, hashed, sig, hash != crypto.Hash(0))/g" src/crypto/rsa/pkcs1v15.go
 sed -i -e "s/boring\.Enabled/boring\.Enabled()/g" ${GO_SOURCES}
 sed -i -e "s/\"crypto\/internal\/boring\"/boring \"crypto\/internal\/backend\"/g" ${GO_SOURCES}
 sed -i -e "s/\"crypto\/internal\/boring\/bbig\"/\"crypto\/internal\/backend\/bbig\"/g" ${GO_SOURCES}
@@ -21,21 +19,37 @@ sed -i -e "s/\!no_openssl/boringcrypto/g" src/crypto/boring/boring.go
 
 # Remove the crypto/internal/boring code as we're replacing it with the openssl backend code.
 rm -rf src/crypto/internal/boring/*.go
-rm -rf src/crypto/internal/boring/bbig
+#rm -rf src/crypto/internal/boring/bbig
 rm src/crypto/boring/notboring_test.go
 rm src/crypto/boring/boring_test.go
+echo """// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package boring provides access to BoringCrypto implementation functions.
+// Check the constant Enabled to find out whether BoringCrypto is available.
+// If BoringCrypto is not available, the functions in this package all panic.
+package boring
+
+import \"github.com/golang-fips/openssl/v2\"
+
+// A BigInt is the raw words from a BigInt.
+// This definition allows us to avoid importing math/big.
+// Conversion between BigInt and *big.Int is in crypto/internal/boring/bbig.
+type BigInt = openssl.BigInt
+""" > src/crypto/internal/boring/doc.go
 
 # Add new openssl backend to module and vendor it.
 cd src
 SCRIPT_DIR=$(readlink -f $(dirname $0))
 CONFIG_DIR=$(readlink -f $(dirname $0)/../config)
 OPENSSL_FIPS_REF=$(go run ${SCRIPT_DIR}/versions.go ${CONFIG_DIR}/versions.json \
-			github.com/golang-fips/openssl-fips)
-go get github.com/golang-fips/openssl-fips@${OPENSSL_FIPS_REF}
+			github.com/golang-fips/openssl)
+go get github.com/golang-fips/openssl/v2@${OPENSSL_FIPS_REF}
 
 replace="${1}"
 if [ -n "${replace}" ]; then
-    echo "replace github.com/golang-fips/openssl-fips => ${replace}" >> go.mod
+	go mod edit -replace github.com/golang-fips/openssl/v2="${replace}"
 fi
 ../bin/go mod tidy
 ../bin/go mod vendor
